4f28d4ce24048f469207b92afb27ee2b2427d729
Caomy0 <99856322+Caomy0@users.noreply.github.com>
2023-11-25 15:16:11 +0700

    Update request.js

--- src/util/request.js
+++ src/util/request.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // import { API_BASE_URL } from '@env';
 
 const request = axios.create({
-    baseURL: 'https://6375fdd2b5f0e1eb85ff2352.mockapi.io/api/v1',
+    baseURL: 'https://6561a8bfdcd355c083240864.mockapi.io/',
 });
 
 export default request;
