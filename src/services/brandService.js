a0d66aee26586ce595311616a2f41f2531ce8f0d
Caomy0 <99856322+Caomy0@users.noreply.github.com>
2023-11-25 15:17:29 +0700

    Update brandService.js

--- src/services/brandService.js
+++ src/services/brandService.js
@@ -2,7 +2,7 @@ import request from '../util/request';
 
 export const getBrands = async () => {
     try {
-        const brands = await request.get('/brands');
+         const brands = await request.get('/brands');
         return brands.data;
     } catch (error) {
         console.log(error);
